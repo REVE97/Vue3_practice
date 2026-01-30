105001b81bffa4810f392070baf89667d780f207
REVE97 <zxcv9675@naver.com>
2026-01-30 17:11:28 +0900

    Feat: select, input date, input radio 태그 관련 추가

--- Vue3_practice/src/pages/HomePage.vue
+++ Vue3_practice/src/pages/HomePage.vue
@@ -82,6 +82,43 @@
   </table>
   </div>
 
+  <hr />
+
+  <div class="select">
+    <h1 style="text-align: center;">SELECT, OPTION</h1>
+    <select name="status">
+      <option value="승인 대기">승인 대기</option>
+      <option value="승인 완료">승인 완료</option>
+      <option value="승인 불가">승인 불가</option>
+    </select>
+  </div>
+
+  <hr />
+
+  <!-- Date 값은 string 값으로 YYYY-MM-DD 형식으로 받아온다 -->
+  <!-- min, max 값을 통해 사용자가 조회 가능한 최소, 최대 날짜를 설정 -->
+  <div class="date">
+    <h1 style="text-align: center;">Date</h1>
+    <input type="date" min="2025-01-01" max="2026-01-01" v-model="selectedDate"/>
+  </div>
+
+  <hr />
+
+  <!-- name 으로 그룹을 구성하여 한 그룹으로 설정함 -->
+  <div class="radio">
+    <h1 style="text-align: center;">Radio</h1>
+    <label>
+      <input type="radio" name="con" value="안전 사고" v-model="con">안전사고
+    </label>
+    <label>
+      <input type="radio" name="con" value="사고 조사" v-model="con">사고조사
+    </label>
+    <label>
+      <input type="radio" name="con" value="정비 요청" v-model="con">정비요청
+    </label>
+  </div>
+
+  <hr />
 
 
 </template>
@@ -95,6 +132,10 @@ const number = ref(10);
 const number_2 = computed(()=>number.value*2);
 const obj = reactive({x:1, y:2, z:3});
 const obj_value = ref(obj.x);
+
+const selectedDate = ref(""); // 날짜 선택 상태
+
+const con = ref(""); // 사고 유형 상태
 
 /* 메서드 */
 

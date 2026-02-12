46589c155936229c4dfebb42d66c6ad47166bb79
REVE97 <zxcv9675@naver.com>
2026-02-12 14:43:42 +0900

    Feat: option value 값 양방향 바인딩 추가

--- Vue3_practice/src/pages/SelectPage.vue
+++ Vue3_practice/src/pages/SelectPage.vue
@@ -4,7 +4,7 @@
   </header>
 
   <div class="select">
-    <select name="status">
+    <select name="status" v-model="status">
       <option value="승인 대기">승인 대기</option>
       <option value="승인 완료">승인 완료</option>
       <option value="승인 불가">승인 불가</option>
@@ -14,6 +14,8 @@
 
 <script setup>
 import { ref, computed, watch, onMounted } from 'vue';
+
+const status = ref("");
 
 </script>
 
